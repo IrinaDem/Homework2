8eb7c4b246fdd1d6669eb041e0fb45f484352341
IrinaDem <starshinova9297@gmail.com>
2024-07-26 23:36:54 +0300

    Add files via upload

---
+++ lesson3/homework2.py
@@ -0,0 +1,6 @@
+number_of_completed_HW =12
+number_of_hours_spent =1.5
+course_name ="Python"
+print(number_of_hours_spent / number_of_completed_HW)
+time_for_one_task =0.125
+print(course_name,"total task:",number_of_completed_HW,"hours spend:",number_of_hours_spent,"average execution time:",time_for_one_task)
